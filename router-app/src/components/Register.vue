a2aae62a9dde3f5297cc69d4d7956628b6daf448
Unknown <ashtonrodriquez@hotmail.com>
2018-07-16 12:53:06 -0400

    Linked backend to a database and created user authentication

--- router-app/src/components/Register.vue
+++ router-app/src/components/Register.vue
@@ -24,7 +24,7 @@ export default {
       await AuthenticationService.register({
         email: this.email,
         password: this.password
-      })              
+      })           
     }
   }
 }
